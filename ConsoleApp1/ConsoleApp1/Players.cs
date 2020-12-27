df8b54e65efa37aabad4a98d23ef11d20c153e1f
YangaL96 <ylomuro12@gmail.com>
2020-12-27 20:57:27 +0000

    Player class with player method implemented with an interface

---
+++ ConsoleApp1/ConsoleApp1/Players.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Players: IPlayerDescription
+    {
+        private string _name;
+        private string _club;
+        private string _position;
+
+        public Players() { }
+        public Players(string name, string club, string position) 
+        {
+            _name = name;
+            _club = club;
+            _position = position;      
+        }
+
+
+        public string PlayerDescription()
+        {
+            return $"{_name} plays as a {_position} at {_club}";
+        }
+
+        static void Main(string[] args)
+        {
+            Players DeclanRice = new Players("Declan Rice", "Midfielder", "West Ham");
+            Console.WriteLine(DeclanRice.PlayerDescription());
+        }
+
+    }
+}
